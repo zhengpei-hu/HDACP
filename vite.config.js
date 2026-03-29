71a30b0d73c18e33c9efea8cd8567583450bfc11
linyao <18787828386@163.com>
2026-03-29 17:12:10 +0800

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/HDACP/',
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
